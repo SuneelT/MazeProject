c4edf347301ee3b27ac1cb52966a0cb07d946482
Simon <lugia584@gmail.com>
2016-05-05 20:32:15 +1000

    More skeleton done

---
+++ src/MazeScreen.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+public class MazeScreen implements Observer, Screen {
+
+	@Override
+	public JPanel getContentPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<JComponent> getMazeListeners() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/PauseScreen.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+public class PauseScreen implements Observer, Screen{
+
+	@Override
+	public JPanel getContentPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<JComponent> getMazeListeners() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
