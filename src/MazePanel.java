98a142a97fd6649afeddd50bf4ba785d4f2769ab
lis4 <lis4li4@gmail.com>
2016-05-07 23:52:06 +1000

    Added files via upload
    added maze panel class

---
+++ src/MazePanel.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *  A visual representation of the maze
+ */
+
+public class MazePanel extends JPanel implements Observer {
+
+    @Override
+    public void update(Observable observable, Object object) {
+    }
+
+}
