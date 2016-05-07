69d311c2eb1106fb8626c8891e198cc01d487d21
SuneelT <st.258@hotmail.com>
2016-05-07 16:11:05 +1000

    Update Edge.java
    Added changes to allow for Maze Generation

--- src/Edge.java
+++ src/Edge.java
@@ -22,8 +22,25 @@ public class Edge {
 		return wall;
 	}
 	
+	public void changeWall(boolean state) {
+		this.wall = state;
+	}
+	
 	public void destroyWall() {
 		this.wall = false;
+		if (from.getX() == to.getX()) {
+			if (from.getY() < to.getY()) {
+				to.getEdge("up").changeWall(false);
+			} else {
+				to.getEdge("down").changeWall(false);
+			}
+		} else if (from.getY() == to.getY()) {
+			if (from.getX() < to.getX()) {
+				to.getEdge("left").changeWall(false);
+			} else {
+				to.getEdge("right").changeWall(false);
+			}
+		}
 	}
 	
 }
