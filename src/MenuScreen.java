326f05f209b535b087f6f5da71000ea1bcfce475
Samantha <samantha.chhoeu@gmail.com>
2016-05-10 10:10:32 +1000

    Update MenuScreen.java

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -10,6 +10,10 @@ public class MenuScreen extends Screen {
 		this.setLayout(new FlowLayout(FlowLayout.CENTER)); //one row, one column for now. in the future, change to 2 or 3 rows.
 		this.setBackground(Color.PINK);
 		JButton playButton = new JButton("Play");
+		playButton.setBackground(Color.WHITE);
+	    	playButton.setForeground(Color.BLACK);
+	    	playButton.setFocusPainted(false);
+	    	playButton.setFont(new Font("Ariel", Font.BOLD, 50));
 		this.add(playButton);
 		playButton.addMouseListener(new MouseAdapter() {
 			@Override
