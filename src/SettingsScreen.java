55c7190428118e9072b8a54b9bc986a66a2e78a1
Samantha <samantha.chhoeu@gmail.com>
2016-05-11 12:11:09 +1000

    Update SettingsScreen.java

--- src/SettingsScreen.java
+++ src/SettingsScreen.java
@@ -7,5 +7,22 @@ public class SettingsScreen extends Screen{
 		// change difficulty
 		
 		// change theme - colours etc
+		
+		// FOR TESTING PURPOSES - will be deleted later
+		setGUI(gui);
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		this.setBackground(new Color(255,204,255));
+		JButton returnButton = new JButton("Return To Game");
+		
+		returnButton.setBackground(Color.WHITE);
+	    	returnButton.setForeground(Color.BLACK);
+		returnButton.setFocusPainted(false);
+		returnButton.setFont(new Font("Ariel", Font.BOLD, 50));
+	    	returnButton.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			getGUI().switchScreen("Maze");
+			}
+		});
 	}
 }
