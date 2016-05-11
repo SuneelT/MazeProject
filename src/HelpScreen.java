74d548c545ed11096cbd7184aac605eceeb5a666
Samantha <samantha.chhoeu@gmail.com>
2016-05-11 11:28:03 +1000

    Update HelpScreen.java

--- src/HelpScreen.java
+++ src/HelpScreen.java
@@ -13,33 +13,56 @@ public class HelpScreen extends Screen {
         GridBagConstraints c = new GridBagConstraints();
         this.setBackground(new Color(255,204,255));
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(new Color(255,204,255));
+        
         menuButton = new JButton("Menu");
         menuButton.setBackground(Color.WHITE);
         menuButton.setForeground(Color.BLACK);
         menuButton.setFocusPainted(false);
         menuButton.setFont(new Font("Ariel", Font.BOLD, 50));
+        buttonPanel.add(menuButton);
         c.gridx = 0;
         c.gridy = 0;
         c.weightx = 0.5;
         c.weighty = 0.5;
         c.insets = new Insets(20, 20, 20, 20);
         c.anchor = GridBagConstraints.PAGE_START;
-        this.add(menuButton, c);
+        
         menuButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 getGUI().switchScreen("Menu");
             }
         });
+        
+        JButton playButton = new JButton("Play Now");
+		playButton.setBackground(Color.WHITE);
+    	playButton.setForeground(Color.BLACK);
+    	playButton.setFocusPainted(false);
+    	playButton.setFont(new Font("Ariel", Font.BOLD, 50));
+    	buttonPanel.add(playButton);
+    	playButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				getGUI().createMaze(25);
+				getModel().resetPlayer();
+				getGUI().switchScreen("Maze");
+			}
+    	});
+    	
+    	this.add(buttonPanel, c);
 
         text = new JLabel("<html>Use the arrow keys on your keyboard to guide your character to the exit.</html>", JLabel.CENTER);
         text.setFont(new Font("Ariel",Font.PLAIN, 30));
         c.gridy = 1;
         c.ipadx = 500;
-        c.ipady = 100;
+        c.ipady = 10;
         c.insets = new Insets(20, 20, 250, 20);
         c.anchor = GridBagConstraints.CENTER;
         this.add(text, c);
+        
+        
     }
 
 }
