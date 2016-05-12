43bbf9ef987783729127cd7a5907a4082a087102
SuneelT <st.258@hotmail.com>
2016-05-12 14:03:34 +1000

    Update Maze.java

--- src/Maze.java
+++ src/Maze.java
@@ -5,7 +5,9 @@ public interface Maze {
 	public int getSize();
 	public boolean isConnected(int x, int y, int dir);
 	
-	
+	public void resetNode ();
+	public boolean hasNext ();
+	public void setNext();
 	// use strategy in generateMaze to have easy, hard, medium algorithm to generate different mazes
 	//public void generateMaze();
 	
