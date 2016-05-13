419de7f016f51753ab25877ad444755aaa976fc5
lis4 <lis4li4@gmail.com>
2016-05-13 18:27:20 +1000

    put buttons in centre of screen

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -4,17 +4,25 @@ import javax.swing.*;
 
 public class MenuScreen extends Screen {
 	private static final long serialVersionUID = -1725872337351590898L;
+	JButton playButton;
+	JButton helpButton;
 
 	public MenuScreen(GUI gui) {
 		setGUI(gui);
-		this.setLayout(new FlowLayout(FlowLayout.CENTER)); //one row, one column for now. in the future, change to 2 or 3 rows.
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		this.setBackground(new Color(255,204,255));
-		JButton playButton = new JButton("Play");
+
+		playButton = new JButton("Play");
 		playButton.setBackground(Color.WHITE);
-	    	playButton.setForeground(Color.BLACK);
-	    	playButton.setFocusPainted(false);
-	    	playButton.setFont(new Font("Ariel", Font.BOLD, 50));
-		this.add(playButton);
+		playButton.setForeground(Color.BLACK);
+		playButton.setFocusPainted(false);
+		playButton.setFont(new Font("Ariel", Font.BOLD, 50));
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 0.5;
+		c.weighty = 0.5;
+		c.insets = new Insets(20, 120, 20, 20);
 		playButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -23,18 +31,22 @@ public class MenuScreen extends Screen {
 				getGUI().switchScreen("Maze");
 			}
 		});
-		JButton helpButton = new JButton("Help");
+		this.add(playButton, c);
+
+		helpButton = new JButton("Help");
 		helpButton.setBackground(Color.WHITE);
 		helpButton.setForeground(Color.BLACK);
 		helpButton.setFocusPainted(false);
 		helpButton.setFont(new Font("Ariel", Font.BOLD, 50));
-		this.add(helpButton);
-		helpButton.addMouseListener(new MouseAdapter() {
+        c.gridx = 1;
+        c.insets = new Insets(20, 20, 20, 120);
+        helpButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				getGUI().switchScreen("Help");
 			}
 		});
+		this.add(helpButton, c);
 	}
 
 }
