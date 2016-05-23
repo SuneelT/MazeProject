01078bd03274fb8dc3356edcdc1e5611beeda660
Simon <lugia584@gmail.com>
2016-05-23 20:33:37 +1000

    made the tiles easier to manage

--- src/CollectedPanel.java
+++ src/CollectedPanel.java
@@ -1,5 +1,6 @@
-import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
 import java.util.Hashtable;
 import java.util.Observable;
 import java.util.Observer;
@@ -9,30 +10,43 @@ import javax.swing.*;
 public class CollectedPanel extends JPanel implements Observer {
 	private static final long serialVersionUID = -4222092480497835994L;
 	private int nCollectables = 0;
-	private Hashtable<Observable, JLabel> collectables = new Hashtable<Observable, JLabel>();
+	private Hashtable<Observable, CollectedTile> collectables = new Hashtable<Observable, CollectedTile>();
 
 	public CollectedPanel() {
-		this.setVisible(false); this.setBackground(new Color(73, 201, 18, 90));
+		this.setVisible(false); this.setOpaque(false);
 	}
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		if (!this.isVisible()) this.setVisible(true);
-		JLabel tmp;
+		CollectedTile tmp;
 		switch((String) arg1) {
 			case "First":
 				this.removeAll();
 				nCollectables = 0;
 			case "Create":
 				this.setLayout(new GridLayout(++nCollectables, 1, 10, 10));
-				tmp = new JLabel(new ImageIcon(((CollectableState) arg0).getUncollectedSprite()));
+				tmp = new CollectedTile();
+				tmp.setSprite(((CollectableState) arg0).getUncollectedSprite());
 				collectables.put(arg0, tmp);
 				this.add(tmp);
 				break;
 			case "Collect":
 				tmp = collectables.get(arg0);
-				tmp.setIcon(new ImageIcon(((CollectableState) arg0).getCollectedSprite()));
+				tmp.setSprite(((CollectableState) arg0).getCollectedSprite());
 				break;
+		}
+	}
+	
+	private class CollectedTile extends JPanel {
+		private static final long serialVersionUID = 1L;
+		private BufferedImage sprite;
+		public CollectedTile() {this.setOpaque(false);}
+		public void setSprite(BufferedImage s) {sprite = s; repaint();}
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(sprite, 0, 0, this.getWidth(), this.getHeight(), null);
 		}
 	}
 
