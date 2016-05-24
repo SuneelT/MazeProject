eb0c9032db7502f6bd71a440d19b46e3cea700e6
Simon <lugia584@gmail.com>
2016-05-24 23:10:05 +1000

    .

--- src/HelpScreen.java
+++ src/HelpScreen.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
